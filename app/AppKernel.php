88cd0f6e3e15c0c0e02238dd070dbe0f93065015
nasreddinebr <berrachednasr@gmail.com>
2017-09-13 19:47:02 +0200

    création du CoreBundle et des layouts, plus les page de la parti views.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Acme\LouvreBundle\AcmeLouvreBundle(),
+            new Acme\CoreBundle\AcmeCoreBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
