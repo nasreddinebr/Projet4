93931f34fa11ac2265590f9eb0fc5c3ecf9985b8
nasreddinebr <berrachednasr@gmail.com>
2017-09-13 16:59:21 +0200

    création des routes et les methodes du controleure

--- src/Acme/LouvreBundle/Controller/LouvreController.php
+++ src/Acme/LouvreBundle/Controller/LouvreController.php
@@ -2,24 +2,45 @@
 
 namespace Acme\LouvreBundle\Controller;
 
-/**
- * Class LouvreController
- *
- * @package \Acme\LouvreBundle\Controller
- */
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class LouvreController extends Controller
 {
     public function indexAction(){
+        //page d'accueil
         $contenue = $this->get('templating')->render('AcmeLouvreBundle:Louvre:index.html.twig');
         return new Response($contenue);
     }
 
-    public function achatBilletAction() {
+    public function achatBilletsAction() {
+        // Page du Formulaire d'achat des billets
         $pageAchat = $this->get('templating')->render('AcmeLouvreBundle:Louvre:achatBillet.html.twig');
         return new Response($pageAchat);
+    }
+
+    public function detailleBilletsAction($id) {
+        /**
+         * $id vaut l'id de la reservation.
+         * Ici en recupérera les données à afficher
+         * depuis la base de donnée.
+         * puis en passera les information à la vue
+         * enfin en envoi les information par mail au client.
+         */
+        return new Response("Affichage des détaile de la reservation id : " . $id);
+    }
+
+    public function enregistrerBilletsAction() {
+        /**
+         * Ici en recupére tous le données depuis le
+         * formulaire d'achat des billets, et puis les enregistrer
+         * sur la base de donnée.
+         */
+        // Générer l'URL de la page de détaille
+        $url = $this->generateUrl('acme_louvre_detaille', array('id' => 7));
+        return $this->redirect($url);
     }
 
 }
