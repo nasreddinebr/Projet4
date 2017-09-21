874288480c4a35a0f6fbdf580020a5ae41a6a54f
nasreddinebr <berrachednasr@gmail.com>
2017-09-21 18:47:18 +0200

    teste d'enregistrement en base de donnée

--- src/OC/LouvreBundle/Controller/LouvreController.php
+++ src/OC/LouvreBundle/Controller/LouvreController.php
@@ -2,10 +2,10 @@
 
 namespace OC\LouvreBundle\Controller;
 
-
+use OC\LouvreBundle\Entity\Clients;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class LouvreController extends Controller
 {
@@ -29,18 +29,40 @@ class LouvreController extends Controller
          * puis en passera les information à la vue
          * enfin en envoi les information par mail au client.
          */
-        return new Response("Affichage des détaile de la reservation id : " . $id);
+        return new Response("Votre enregistrement à bien été effectuer" . $id);
     }
 
-    public function enregistrerBilletsAction() {
+    public function enregistrerBilletsAction(Request $request) {
         /**
-         * Ici en recupére tous le données depuis le
+         * Ici en recupére tous les données depuis le
          * formulaire d'achat des billets, et puis les enregistrer
          * sur la base de donnée.
          */
-        // Générer l'URL de la page de détaille
-        $url = $this->generateUrl('oc_louvre_detaille', array('id' => 7));
-        return $this->redirect($url);
+        //création de l'entitee Clients
+        $client = new Clients();
+        $client->setNom('Berrached');
+        $client->setPrenom('Nasreddine');
+        $client->setDateNaissance(new \DateTime('1980-08-17'));
+        $client->setPays('Maroc');
+        $client->setIdBillet(1);
+        $client->setIdTarif(1);
+
+        //onrecupére le gestionnaire de donnée
+        $em = $this->getDoctrine()->getManager();
+
+        // On persiste l'objet
+        $em->persist($client);
+
+        // On demande la mise a jour
+        $em->flush();
+
+        if ($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('notice', 'Client enregistrer');
+
+            return $this->redirectToRoute('oc_louvre_detaille', array('id' => 1));
+
+        }
+        return $this->render('OCLouvreBundle:Louvre:achatBillet.html.twig');
     }
 
 }
